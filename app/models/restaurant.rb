528f2d703a576942eae687621885c5516d9ec9ba
izumi32 <k.izumi721@gmail.com>
2021-11-15 22:14:15 +0900

    seeds作成

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,6 +4,6 @@ class Restaurant < ApplicationRecord
   has_many :line_foods, through: :foods
 
   validates :name, :fee, :time_required, presence: true
-  validates :name, length: ( maximum: 30 )
+  validates :name, length: { maximum: 30 }
   validates :fee, numericality: { greater_than: 0}
 end
